089ecad87852f8730ae7c051565a9d77edc2c306
leo <leocrapart@yahoo.fr>
2023-08-21 23:36:43 +0200

    init

---
+++ src/tri_fusion/core.clj
@@ -0,0 +1,21 @@
+(ns tri-fusion.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+
+(+ 1 1)
+
+(count [1, 2, 3])
+
+(defn tri-fusion [list]
+
+
+  )
+
+(tri-fusion [3, 2, 1])
+
+
